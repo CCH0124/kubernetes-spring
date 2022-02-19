861abe72e60bb92af0a0bb1266be44f3d07c3291
CCH0124 <cchong0124@gmail.com>
2022-02-19 18:22:51 +0800

    add constructor

--- src/main/java/example/k8s/cch/k8sdemo/controller/IpController.java
+++ src/main/java/example/k8s/cch/k8sdemo/controller/IpController.java
@@ -10,7 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class IpController {
 
     @Value("${COMMITHASH:}")
